b463fc53508c59f5a307f9e7eb0ff26c504117c1
Logan Fouts <112691772+Logan-Fouts@users.noreply.github.com>
2025-08-29 15:04:28 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -90,7 +90,7 @@ print_status "Pi-hole installed."
 print_section "Node.js Verification"
 if ! command -v node &> /dev/null; then
     print_warning "Node.js not found, installing nodejs..."
-    run_cmd "sudo apt install -y nodejs-legacy"
+    run_cmd "sudo apt install -y nodejs"
     print_status "nodejs-legacy installed."
 else
     print_status "Node.js is present."
